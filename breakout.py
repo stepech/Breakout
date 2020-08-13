58a2317c59467fb632284a3a1f20ae1a1b0a08c4
stepech <step3ch@gmail.com>
2020-08-13 09:17:14 +0200

    Initial commit

---
+++ breakout.py
@@ -0,0 +1,89 @@
+"""
+File: breakout.py
+-----------------
+This program implements the game Breakout!  The user controls a paddle
+moving horizontally with the mouse, and the user must bounce the ball
+to make it collide and remove bricks from the screen.  The user has
+3 turns.  If the ball falls below the bottom of the screen, the user
+loses a turn.  If the user removes all bricks before their turns
+run out, they win!
+"""
+
+import math
+from graphics import Canvas
+import random
+import time
+
+"""
+Dimensions of the canvas, in pixels
+These should be used when setting up the initial size of the game,
+but in later calculations you should use canvas.get_canvas_width() and 
+canvas.get_canvas_height() rather than these constants for accurate size information.
+"""
+CANVAS_WIDTH = 420
+CANVAS_HEIGHT = 600
+
+# Stage 1: Set up the Bricks
+
+# Number of bricks in each row
+NBRICK_COLUMNS = 10
+
+# Number of rows of bricks
+NBRICK_ROWS = 10
+
+# Separation between neighboring bricks, in pixels
+BRICK_SEP = 4
+
+# Width of each brick, in pixels
+BRICK_WIDTH = math.floor((CANVAS_WIDTH - (NBRICK_COLUMNS + 1.0) * BRICK_SEP) / NBRICK_COLUMNS)
+
+# Height of each brick, in pixels
+BRICK_HEIGHT = 8
+
+# Offset of the top brick row from the top, in pixels
+BRICK_Y_OFFSET = 70
+
+# Stage 2: Create the Bouncing Ball
+
+# Radius of the ball in pixels
+BALL_RADIUS = 10
+
+# The ball's vertical velocity.
+VELOCITY_Y = 6.0
+
+# The ball's minimum and maximum horizontal velocity; the bounds of the
+# initial random velocity that you should choose (randomly +/-).
+VELOCITY_X_MIN = 2.0
+VELOCITY_X_MAX = 6.0
+
+# Animation delay or pause time between ball moves (in seconds)
+DELAY = 1 / 60
+
+# Stage 3: Create the Paddle
+
+# Dimensions of the paddle
+PADDLE_WIDTH = 60
+PADDLE_HEIGHT = 10
+
+# Offset of the paddle up from the bottom
+PADDLE_Y_OFFSET = 30
+
+# Stage 5: Polish and Finishing Up
+
+# Number of turns
+NTURNS = 3
+
+BOUNCE_SOUND = "bounce.au"
+
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    canvas.set_canvas_title("Breakout")
+
+    # TODO: your code here!
+
+    canvas.mainloop()
+
+
+if __name__ == '__main__':
+    main()
